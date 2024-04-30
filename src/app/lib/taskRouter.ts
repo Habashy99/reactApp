a662a42bbf1abc6d2a5d3f08bd95ae5fbace5977
Abdelrahman Habashy <abdelrahmanhabashy971@gmail.com>
2024-04-30 17:23:17 +0200

    delete

--- src/app/lib/taskRouter.ts
+++
@@ -1,8 +0,0 @@
-export default class TaskRouter {
-    static getTasks() {
-        return fetch(`http://localhost:8050/tasks`, { cache: 'no-store' }).then((response) => {
-            return response.json();
-        });
-    }
-}
-
